9b514bce1fdaac9442bfdb04d6e27a27cc573d3e
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2024-05-21 19:08:04 -0400

    Adicionando a camada de dominio

---
+++ DesafioApiSantanderDevWeek2024/src/main/java/me/dio/domain/model/News.java
@@ -0,0 +1,14 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "news")
+public class News extends BaseItem {
+
+}
